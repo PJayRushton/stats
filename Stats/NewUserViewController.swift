28c475d789d0e4590a65a78ee3449528e2dede2b
PJayRushton <parker.rushton@octanner.com>
2017-03-27 09:49:18 -0600

    Adds NewUser Xib

---
+++ Stats/NewUserViewController.swift
@@ -0,0 +1,17 @@
+//
+//  NewUserViewController.swift
+//  Stats
+//
+//  Created by Parker Rushton on 3/27/17.
+//  Copyright © 2017 AppsByPJ. All rights reserved.
+//
+
+import UIKit
+
+class NewUserViewController: XIBViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Stats/XIBViewController.swift
@@ -0,0 +1,22 @@
+//
+//  XIBViewController.swift
+//  Stats
+//
+//  Created by Parker Rushton on 3/27/17.
+//  Copyright © 2017 AppsByPJ. All rights reserved.
+//
+
+import UIKit
+
+class XIBViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}

b02279656ad239a13c5ac2375bd688277dcc6efc
PJayRushton <parker.rushton@octanner.com>
2017-03-29 08:23:09 -0600

    Adds TeamHeaderCell

---
+++ Stats/TeamHeaderCell.swift
@@ -0,0 +1,28 @@
+//
+//  TeamHeaderCell.swift
+//  Stats
+//
+//  Created by Parker Rushton on 3/29/17.
+//  Copyright © 2017 AppsByPJ. All rights reserved.
+//
+
+import UIKit
+
+class TeamHeaderCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var editButton: UIButton!
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var switchTeamButton: UIButton!
+    
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func editButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func switchTeamButtonPressed(_ sender: UIButton) {
+    }
+    
+}

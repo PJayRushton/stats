2b6623782f9581bfb35c3ff015d9ec85059072e1
Parker Rushton <parker.rushton@octanner.com>
2017-08-28 11:30:48 -0600

    Deletes season data

--- Stats/DeleteSeason.swift
+++ Stats/DeleteSeason.swift
@@ -20,14 +20,30 @@ struct DeleteSeason: Command {
     func execute(state: AppState, core: Core<AppState>) {
         networkAccess.deleteObject(at: season.ref) { result in
             if case .success = result {
-                self.deleteGames(core: core)
+                self.deleteGames(core)
+                self.deleteAtBats(core)
+                self.deleteFromPlayers(core)
             }
         }
     }
     
-    func deleteGames(core: Core<AppState>) {
+    func deleteGames(_ core: Core<AppState>) {
         let gamesToDelete = core.state.gameState.games(of: season)
         gamesToDelete.forEach( { core.fire(command: DeleteGame($0)) })
+    }
+    
+    func deleteAtBats(_ core: Core<AppState>) {
+        let atBatsToDelete = core.state.atBatState.atBats.filter { $0.seasonId == season.id }
+        atBatsToDelete.forEach { core.fire(command: DeleteObject($0)) }
+    }
+    
+    func deleteFromPlayers(_ core: Core<AppState>) {
+        let players = core.state.playerState.players(for: season.teamId)
+        players.forEach { player in
+            var updatedPlayer = player
+            updatedPlayer.seasons[seasonId] = nil
+            core.fire(command: SetObject(updatedPlayer))
+        }
     }
     
 }
